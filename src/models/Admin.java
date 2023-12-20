885f8f770a9534220525f6c7754d2d533940c0f1
kevin chayadi <kevinchyd13@gmail.com>
2023-12-20 20:27:14 +0700

    Remove all files

--- src/models/Admin.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public class Admin extends User{
-
-	public Admin(String email, String password, String role) {
-		super(email, password, "Admin");
-	}
-
-}
--- src/models/Customers.java
+++
@@ -1,8 +0,0 @@
-package models;
-
-public class Customers extends User{
-
-	public Customers(String email, String password) {
-		super(email, password, "Customer");
-	}
-}
